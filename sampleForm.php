197ac0bc9fd33aeed77e74ee2a8ab09960265b5f
Rebecca Follman <31415839+RFollman@users.noreply.github.com>
2018-03-09 10:34:22 -0500

    validation

--- sampleForm.php
+++ sampleForm.php
@@ -6,12 +6,12 @@
 		include 'resources/bslinks.php';
 
 		//get authors data
-		$a = "select * from authors order by l_name, f_name";
+		/*$a = "select * from authors order by l_name, f_name";
 		require_once 'liblogin.php';
 		$conn = new mysqli($hostname, $user, $pword, $database, 3306, '/Applications/MAMP/tmp/mysql/mysql.sock');
 		if ($conn->connect_error) die($conn->connect_error);
-		$result = $conn->query($a);
-		echo "<h1>Hello</h1>";
+		$result = $conn->query($a);*/
+		/*echo "<h1>Hello</h1>";*/
 	 ?>	
 	 <link rel="stylesheet" href="css/main-php.css">
 </head>
@@ -24,10 +24,10 @@
 				<div class="form-group">
 					<label for="f_name" class="control-label col-sm-3">Author's Name</label>
 					<div class="col-sm-3">
-						<input type="text" class="form-control" id="f_name" name="f_name" placeholder="First Name">
+						<input type="text" class="form-control" id="f_name" name="f_name" placeholder="First Name" maxlength="25">
 					</div>
 					<div class="col-sm-4">
-						<input type="text" class="form-control" id="l_name" name="l_name" placeholder="Last Name">
+						<input type="text" class="form-control" id="l_name" name="l_name" placeholder="Last Name" maxlength="35">
 					</div>
 				</div>
 				<div class="form-group">
